492f1527020d20d6a51d0025fa8509e220be5ac2
UzairFahima3 <uzairfahima3@gmail.com>
2022-09-15 17:12:59 +0500

    Active Records for Storing Images

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   
   has_many :articles, :dependent => :destroy
   has_many :comments, :dependent => :destroy
-  # validates :user_name, presence: true  
+  # validates :user_name, presence: true 
+  has_one_attached :image, :dependent => :destroy 
 end
